572692eaa879fb8df11636961886cc03887c21d3
David Gwyer <d.v.gwyer@gmail.com>
2020-01-14 19:22:48 +0000

    Added readme details

--- examples/keys.js
+++ examples/keys.js
@@ -1,8 +1,8 @@
 module.exports = {
   FS__API_APP_ID: 0,
-  FS__API_DEV_ID: 4301,
+  FS__API_DEV_ID: 1234,
   FS__API_USER_ID: 0,
-  FS__API_PLUGIN_ID : 5235,
-  FS__API_PUBLIC_KEY: 'pk_c516c0e6c51a9ffd2c113283a1db6',
-  FS__API_SECRET_KEY: 'sk_z;&UU]qF-M+J{)4a%F!h?xmhKF!iA' 
+  FS__API_PLUGIN_ID : 1234,
+  FS__API_PUBLIC_KEY: 'pk_*****************************',
+  FS__API_SECRET_KEY: 'sk_*****************************' 
 };
